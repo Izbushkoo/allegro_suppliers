2edcfd9131c1cbccbe4adc099e4515a7df1b14d0
Izbushko <izbaizbushka@gmail.com>
2024-06-19 14:07:20 +0300

    add callback_url to update config

--- app/schemas/pydantic_models.py
+++ app/schemas/pydantic_models.py
@@ -10,6 +10,7 @@ class UpdateConfig(BaseModel):
     multiplier: Optional[int | float] = Field(default=1)
     suppliers_to_update: Optional[List[str]] = Field(default=None)
     oferta_ids_to_process: Optional[List[str]] = Field(default=None)
+    callback_url: Optional[str] = Field(default=None)
     allegro_token_id: str
 
 
