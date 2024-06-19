673571735d42192870a4f8767fe4b21f7f42f33d
Izbushko <izbaizbushka@gmail.com>
2024-06-19 14:21:43 +0300

    add resource_id to update config

--- app/schemas/pydantic_models.py
+++ app/schemas/pydantic_models.py
@@ -8,9 +8,10 @@ from fastapi import WebSocket
 class UpdateConfig(BaseModel):
 
     multiplier: Optional[int | float] = Field(default=1)
-    suppliers_to_update: Optional[List[str]] = Field(default=None)
-    oferta_ids_to_process: Optional[List[str]] = Field(default=None)
-    callback_url: Optional[str] = Field(default=None)
+    suppliers_to_update: Optional[List[str] | None] = Field(default=None)
+    oferta_ids_to_process: Optional[List[str] | None] = Field(default=None)
+    callback_url: Optional[str | None] = Field(default=None)
+    resourse_id: Optional[str | None] = Field(default=None)
     allegro_token_id: str
 
 
