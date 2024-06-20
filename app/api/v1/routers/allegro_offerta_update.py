cef4d7be79086949cb8f1d3fe5dd88e4add49579
Izbushko <izbaizbushka@gmail.com>
2024-06-20 15:57:02 +0300

    tesst

--- app/api/v1/routers/allegro_offerta_update.py
+++ app/api/v1/routers/allegro_offerta_update.py
@@ -137,14 +137,14 @@ async def update_as_task(update_config: UpdateConfig):
         # except Exception as e:
         #     await callback_manager.send_error_callback_async(f"Error with parsing {supplier} data. Try later.")
         #     ToLog.write_error(f"{e}")
-        else:
-            await fetch_and_update_allegro(
-                database,
-                filtered_objects,
-                allegro_token,
-                oferta_ids_to_process=oferta_ids_to_process,
-                callback_manager=callback_manager
-            )
+        # else:
+        await fetch_and_update_allegro(
+            database,
+            filtered_objects,
+            allegro_token,
+            oferta_ids_to_process=oferta_ids_to_process,
+            callback_manager=callback_manager
+        )
 
     ToLog.write_basic("Update Finished")
     await callback_manager.send_finish_callback_async("Update Finished")
