0d4d87821834be8fcb6ba16c468bfe5205d3224b
Izbushko <izbaizbushka@gmail.com>
2024-06-20 09:17:55 +0300

    some test

--- app/services/modules/AlegroApiManager.py
+++ app/services/modules/AlegroApiManager.py
@@ -49,19 +49,6 @@ async def update_offers(offers_array, access_token: str, callback_manager: Callb
                 id_ = offer.get('id')
                 if oferta_ids_to_process and id_ not in oferta_ids_to_process:
                     continue
-                ToLog.write_basic(f"{id_} testing")
-                await sleep(1000)
-                # result = await client.post(
-                #     url=callback_manager.url, json=json.dumps({
-                #         "resource_id": callback_manager.resource_id,
-                #         "status": "OK",
-                #         "message": "testing .... "
-                #     }), headers={
-                #         "Content-Type": "application/json"
-                #     }
-                # )
-                # await callback_manager.send_ok_callback_async(client, "testing ....")
-                await callback_manager.send_ok_callback_async("testing kind of a long message with some additional information")
 
         #         stock = offer.get('stock')
         #         price = offer.get('price')
