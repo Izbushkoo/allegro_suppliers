48ec78ed49f5bfdd181115343fa9ea8e1591bee6
Izbushko <izbaizbushka@gmail.com>
2024-06-21 13:32:46 +0300

    add api key for cronjobs

--- app/api/v1/routers/scheduler_routes.py
+++ app/api/v1/routers/scheduler_routes.py
@@ -7,8 +7,8 @@ from app.scheduler_service.scheduler_tasks import stop_task, add_task
 from app.loggers import ToLog
 
 
-# router = APIRouter(dependencies=[Depends(deps.get_api_token)])
-router = APIRouter()
+router = APIRouter(dependencies=[Depends(deps.get_api_token)])
+# router = APIRouter()
 
 
 @router.post('/task_start')
