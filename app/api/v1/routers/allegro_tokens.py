cb95bc5d21acca8023e35d234c97c4211fd3433d
Izbushko <izbaizbushka@gmail.com>
2024-06-26 16:16:59 +0300

    added route for get certain token

--- app/api/v1/routers/allegro_tokens.py
+++ app/api/v1/routers/allegro_tokens.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api import deps
-from app.services.allegro_token import get_tokens_list, get_token_by_name, insert_token, delete_token
+from app.services.allegro_token import get_tokens_list, get_token_by_name, insert_token, delete_token, get_token_by_id
 from app.schemas.token import TokenOfAllegro
 from app.models.database_models import AllegroToken
 from app.loggers import ToLog
@@ -28,6 +28,13 @@ async def add_account(account_data: AllegroToken, database: AsyncSession = Depen
     written_token = await insert_token(database, account_data)
 
     return TokenOfAllegro(**written_token.model_dump(exclude_none=True))
+
+
+@router.get("/get_by_id")
+async def add_account(token_id: str, database: AsyncSession = Depends(deps.get_db_async)):
+
+    ToLog.write_access(f"Access to allegro token get by ID")
+    return get_token_by_id(database, token_id)
 
 
 @router.delete("/delete")
