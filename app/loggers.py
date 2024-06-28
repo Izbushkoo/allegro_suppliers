8a18fcd1688c1de9808fd1728f1ab20355fd4162
Izbushko <izbaizbushka@gmail.com>
2024-06-28 15:32:25 +0300

    bigger log files

--- app/loggers.py
+++ app/loggers.py
@@ -17,21 +17,21 @@ def setup_loggers():
 
     errors = logging.getLogger("errors")
     file_error_handler = RotatingFileHandler(os.path.join(base_path, "error_log.log"),
-                                             maxBytes=10*1024*1024, backupCount=2)
+                                             maxBytes=100*1024*1024, backupCount=2)
     errors.setLevel(level=logging.ERROR)
     errors.addHandler(file_error_handler)
     errors.addHandler(stream_handler)
 
     access = logging.getLogger("access")
     access_file_handler = RotatingFileHandler(os.path.join(base_path, "access_log.log"),
-                                              maxBytes=10*1024*1024, backupCount=2)
+                                              maxBytes=100*1024*1024, backupCount=2)
     access_file_handler.setFormatter(formatter)
     access.setLevel(level=logging.INFO)
     access.addHandler(access_file_handler)
     # access.addHandler(stream_handler)
 
     debug_file_handler = RotatingFileHandler(os.path.join(base_path, "debug_log.log"),
-                                             maxBytes=10*1024*1024, backupCount=2)
+                                             maxBytes=100*1024*1024, backupCount=2)
 
     logging.basicConfig(handlers=(debug_file_handler, stream_handler), level=logging.DEBUG)
     httpcore_logger = logging.getLogger("httpcore")
