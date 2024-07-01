2d04bd30b236e9004905f5b66dec1397230c72c3
Izbushko <izbaizbushka@gmail.com>
2024-07-01 09:36:49 +0300

    add formater to debug logger

--- app/loggers.py
+++ app/loggers.py
@@ -32,7 +32,7 @@ def setup_loggers():
 
     debug_file_handler = RotatingFileHandler(os.path.join(base_path, "debug_log.log"),
                                              maxBytes=100*1024*1024, backupCount=2)
-
+    debug_file_handler.setFormatter(formatter)
     logging.basicConfig(handlers=(debug_file_handler, stream_handler),
                         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
                         level=logging.DEBUG)
