b594fe79b531494ab298849ebf23be661044aff0
Izbushko <izbaizbushka@gmail.com>
2024-08-06 13:09:11 +0300

    add handle any exception in handle single

--- app/services/synchro_products_funcs.py
+++ app/services/synchro_products_funcs.py
@@ -13,30 +13,32 @@ from app.services.modules.AlegroApiManager import search_product_by_ean_return_f
 async def handle_single_product(supplier_product, allegro_access_token):
 
     ean = supplier_product["ean"]
-    if ean:
-        try:
-            found_product = await search_product_by_ean_return_first(ean, allegro_access_token)
-        except (httpx.TimeoutException, httpx.RemoteProtocolError) as err:
-            ToLog.write_error(f"Error {err} \n skiped product with ean {ean}")
-            return None
+    try:
+        if ean:
+            try:
+                found_product = await search_product_by_ean_return_first(ean, allegro_access_token)
+            except httpx.TimeoutException as err:
+                return None
 
-        if found_product:
-            if supplier_product["stock"] > 0:
-                product_to_work_with = {
-                    **supplier_product,
-                    "allegro_product_id": found_product["id"],
-                    "category_id": found_product["category"]["id"],
-                    "product_name": found_product["name"]
-                }
-                allegro_response = await create_single_offer(product_to_work_with, access_token=allegro_access_token)
-                if allegro_response:
-                    product_to_work_with["allegro_oferta_id"] = allegro_response["id"]
-                    product_to_work_with["allegro_we_sell_it"] = True
-                    product_to_work_with.pop("price")
-                    product_to_work_with.pop("stock")
+            if found_product:
+                if supplier_product["stock"] > 0:
+                    product_to_work_with = {
+                        **supplier_product,
+                        "allegro_product_id": found_product["id"],
+                        "category_id": found_product["category"]["id"],
+                        "product_name": found_product["name"]
+                    }
+                    allegro_response = await create_single_offer(product_to_work_with, access_token=allegro_access_token)
+                    if allegro_response:
+                        product_to_work_with["allegro_oferta_id"] = allegro_response["id"]
+                        product_to_work_with["allegro_we_sell_it"] = True
+                        product_to_work_with.pop("price")
+                        product_to_work_with.pop("stock")
 
-                    ToLog.write_basic(f"Created offer with id {product_to_work_with['allegro_oferta_id']}")
-                    return product_to_work_with
+                        ToLog.write_basic(f"Created offer with id {product_to_work_with['allegro_oferta_id']}")
+                        return product_to_work_with
+    except Exception as er:
+        ToLog.write_error(f"Error {er} \n skiped product with ean {ean}")
 
 
 async def process_complete_synchro_task(synchro_config: SynchronizeOffersRequest, access_token, products,
@@ -50,7 +52,8 @@ async def process_complete_synchro_task(synchro_config: SynchronizeOffersRequest
                 tasks.append(task)
         results = await asyncio.gather(*tasks)
         all_results = [result for result in results if result]
-        ToLog.write_basic(f"Added to Mongo {len(all_results)} documents")
-        await MongoManager.append_bulks(all_results, synchro_config.supplier)
+        if all_results:
+            await MongoManager.append_bulks(all_results, synchro_config.supplier)
+            ToLog.write_basic(f"Added to Mongo {len(all_results)} documents")
 
     ToLog.write_basic(f"Synchronization Finished")
