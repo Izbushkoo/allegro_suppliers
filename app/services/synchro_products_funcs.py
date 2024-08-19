c5db714e55f83a72e35ce8747553b7227c5dc60c
Izbushko <izbaizbushka@gmail.com>
2024-08-19 16:58:28 +0300

    offer to deactivate log

--- app/services/synchro_products_funcs.py
+++ app/services/synchro_products_funcs.py
@@ -86,10 +86,10 @@ async def make_single_oferta_check(product_from_mongo, access_token):
 
     ean = product_from_mongo["ean"]
     allegro_product_id = product_from_mongo["allegro_product_id"]
-    ToLog.write_basic(f"product id {allegro_product_id}")
     offer_id = product_from_mongo["allegro_oferta_id"]
     products = await search_product_by_ean(ean, access_token)
     allegro_product_details = await get_product_details(allegro_product_id, access_token)
+
     allegro_eans = None
     for param in allegro_product_details["parameters"]:
         if param["id"] == "225693" or param["name"] == "EAN (GTIN)":
@@ -97,9 +97,11 @@ async def make_single_oferta_check(product_from_mongo, access_token):
 
     if allegro_eans:
         if len(allegro_eans["values"]) != 1:
+            ToLog.write_basic(f"Offer id to deactivate {offer_id}")
             return {"id": offer_id}
 
     if len(products) != 1:
+        ToLog.write_basic(f"Offer id to deactivate {offer_id}")
         return {"id": offer_id}
 
 
